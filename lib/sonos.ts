0dca04bb158c3d676f7b30414b8d208ba5ad3cc4
James Booth <jabooth@gmail.com>
2016-12-19 11:48:32 +0000

    clean up spotify metadata handling

--- lib/sonos.ts
+++ lib/sonos.ts
@@ -51,6 +51,7 @@ const SEARCH_TYPES_TO_SPECIFIER: {[P in SearchType]: string } =  {
   share: 'S:',
 }
 
+  const SPOTIFY_METADATA_TEMPLATE = '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="##SPOTIFYURI##" restricted="true"><dc:title>##RESOURCETITLE##</dc:title><upnp:class>##SPOTIFYTYPE##</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON3079_X_#Svc3079-0-Token</desc></item></DIDL-Lite>'
 
 /**
  * Dependencies
@@ -89,7 +90,6 @@ function parseXML(str: string) {
   })
 }
 
-
 /**
  * Wrap in UPnP Envelope
  */
@@ -114,41 +114,48 @@ function isSpotifyUri(uri: string) {
   return uri.startsWith('spotify')
 }
 
+
+function spotifyMetadata(spotifyUri: string, spotifyType: string, resourceTitle = '') {
+      return SPOTIFY_METADATA_TEMPLATE
+                .replace('##SPOTIFYURI##', spotifyUri)
+                .replace('##RESOURCETITLE##', resourceTitle)
+                .replace('##SPOTIFYTYPE##', spotifyType)
+}
+
 /**
- * Creates object with uri and metadata from Spotify track uri
+ * Creates object with uri and metadata from Spotify track uri.
+ * @param radioTitle Optional, if the URI provided is a radio URI, the radio title can be manually set
  */
-function optionsFromSpotifyUri(uri: string, title?: string) {
+function optionsFromSpotifyUri(uri: string, radioTitle = 'Artist Radio') {
   const spotifyIds = uri.split(':')
   if (Array.isArray(spotifyIds) && spotifyIds.length < 3) {
     return { uri }
   }
   const spotifyUri = uri.replace(/:/g, '%3a')
-  const meta = '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="##SPOTIFYURI##" restricted="true"><dc:title>##RESOURCETITLE##</dc:title><upnp:class>##SPOTIFYTYPE##</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON3079_X_#Svc3079-0-Token</desc></item></DIDL-Lite>'
   if (uri.startsWith('spotify:track:')) {
     return {
       uri: spotifyUri,
-      metadata: meta.replace('##SPOTIFYURI##', '00032020' + spotifyUri).replace('##RESOURCETITLE##', '').replace('##SPOTIFYTYPE##', 'object.item.audioItem.musicTrack'),
+      metadata: spotifyMetadata('00032020' + spotifyUri, 'object.item.audioItem.musicTrack'),
     }
   } else if (uri.startsWith('spotify:album:')) {
     return {
       uri: 'x-rincon-cpcontainer:0004206c' + spotifyUri,
-      metadata: meta.replace('##SPOTIFYURI##', '0004206c' + spotifyUri).replace('##RESOURCETITLE##', '').replace('##SPOTIFYTYPE##', 'object.container.album.musicAlbum'),
+      metadata: spotifyMetadata('0004206c' + spotifyUri, 'object.container.album.musicAlbum'),
     }
   } else if (uri.startsWith('spotify:artistTopTracks:')) {
     return {
       uri: 'x-rincon-cpcontainer:000e206c' + spotifyUri,
-      metadata: meta.replace('##SPOTIFYURI##', '000e206c' + spotifyUri).replace('##RESOURCETITLE##', '').replace('##SPOTIFYTYPE##', 'object.container.playlistContainer'),
+      metadata: spotifyMetadata('000e206c' + spotifyUri, 'object.container.playlistContainer'),
     }
   } else if (uri.startsWith('spotify:user:')) {
     return {
       uri: 'x-rincon-cpcontainer:0006206c' + spotifyUri,
-      metadata: meta.replace('##SPOTIFYURI##', '0006206c' + spotifyUri).replace('##RESOURCETITLE##', '').replace('##SPOTIFYTYPE##', 'object.container.playlistContainer'),
+      metadata: spotifyMetadata('0006206c' + spotifyUri, 'object.container.playlistContainer'),
     }
   } else if (uri.startsWith('spotify:artistRadio:')) {
-    const radioTitle = (title !== undefined) ? title : 'Artist Radio'
     return {
-      uri: spotifyUri,
-      metadata: '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="000c206c' + spotifyUri + '" restricted="true"><dc:title>' + radioTitle + '</dc:title><upnp:class>object.item.audioItem.audioBroadcast.#artistRadio</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON3079_X_#Svc3079-0-Token</desc></item></DIDL-Lite>',
+      uri: 'x-sonosapi-radio:'  + spotifyUri + '?sid=12&amp;flags=8300&amp;sn=1',
+      metadata: spotifyMetadata('000c206c' + spotifyUri, 'object.item.audioItem.audioBroadcast.#artistRadio', radioTitle),
     }
   } else {
     return { uri }
@@ -503,10 +510,8 @@ export class Sonos {
    * Add a song from spotify to the queue
    * @param  trackId      The spotify track ID
    */
-  addSpotify(trackId) {
-    const uri = 'x-sonos-spotify:spotify%3atrack%3a' + trackId
-    const metadata = '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="00032020spotify%3atrack%3a' + trackId + '" restricted="true"><dc:title></dc:title><upnp:class>object.item.audioItem.musicTrack</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON3079_X_#Svc3079-0-Token</desc></item></DIDL-Lite>'
-    return this.queue({ uri, metadata })
+  queueSpotifyTrack(trackId: string) {
+    return this.queue(optionsFromSpotifyUri(`spotify:track:${trackId}`))
   }
 
   /**
@@ -514,19 +519,17 @@ export class Sonos {
    */
   async playSpotifyRadio(artistId: string, artistName: string) {
     log('Sonos.playSpotifyRadio(%j, %j)', artistId, artistName)
-    const options = optionsFromSpotifyUri('spotify:artistRadio:' + artistId, artistName)
-    const bodyContent = '<CurrentURI>x-sonosapi-radio:' + options.uri + '?sid=12&amp;flags=8300&amp;sn=1</CurrentURI><CurrentURIMetaData>' + htmlEntities(options.metadata) + '</CurrentURIMetaData>'
-    return this.transportCommand('SetAVTransportURI', bodyContent)
+    return this.queueNext(optionsFromSpotifyUri('spotify:artistRadio:' + artistId, artistName))
   }
 
   /**
    * Queue a Song Next
    * @param   uri      URI to Audio Stream
-   * @param   metadata      optional metadata about the audio stream
+   * @param   metadata  optional metadata about the audio stream
    */
-  queueNext(uri: string, metadata = '') {
-    log(`Sonos.queueNext(${uri}, ${metadata})`)
-    const bodyContent = `<CurrentURI>${uri}</CurrentURI><CurrentURIMetaData>${htmlEntities(metadata)}</CurrentURIMetaData></u:SetAVTransportURI>`
+  queueNext(opts: { uri: string, metadata?: string}) {
+    log(`Sonos.queueNext(${opts})`)
+    const bodyContent = `<CurrentURI>${opts.uri}</CurrentURI><CurrentURIMetaData>${htmlEntities(opts.metadata ? opts.metadata : '')}</CurrentURIMetaData></u:SetAVTransportURI>`
     return this.transportRequest('SetAVTransportURI', bodyContent)
   }
 
