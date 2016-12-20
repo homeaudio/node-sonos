8d2ecfaf103c235c0ef4ec666e80a74f6a2e214b
James Booth <jabooth@gmail.com>
2016-12-20 08:51:26 +0000

    add next ts example

---
+++ examples/next.ts
@@ -0,0 +1,8 @@
+import { Sonos } from '../'
+const sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
+
+sonos.next().then((nexted) => {
+  if (nexted) {
+    console.log('Complete')
+  }
+})
